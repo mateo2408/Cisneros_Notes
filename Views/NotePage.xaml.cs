5625e57d4a3d886a516c3edc283df10e5e5ac984
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-06-13 14:38:25 -0500

    Actualizacion de NotePage

--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -7,8 +7,10 @@ public partial class NotePage : ContentPage
     {
         InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
@@ -23,5 +25,18 @@ public partial class NotePage : ContentPage
             File.Delete(_fileName);
 
         TextEditor.Text = string.Empty;
+    }
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 }
