15f664e5a46e660f7c5387a11bd235dfbce32988
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-06-13 14:09:01 -0500

    Codificacion de AboutPage

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -12,4 +12,9 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
 }
