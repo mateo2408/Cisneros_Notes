5cffb0868bc600d04821e5abba77b6aa714be2b7
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-06-13 14:30:39 -0500

    Correcion de nombre, movimiento de archivos, creacion de Models, Creacion de Note/Models

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cisneros_Notes;
+namespace Cisneros_Notes.Views;
 
 public partial class AboutPage : ContentPage
 {
