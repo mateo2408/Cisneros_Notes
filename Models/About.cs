24644763524673ea86834c796c665747af93d2e7
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-06-13 14:36:05 -0500

    Actualizacion de AboutPage.Views, actualizacion de Mabout/Models

--- Models/About.cs
+++ Models/About.cs
@@ -2,8 +2,11 @@ namespace Cisneros_Notes.Models;
 
 internal class About
 {
-    public string Title => AppInfo.Name;
+    public string Title => "Mateo Cisneros";
     public string Version => AppInfo.VersionString;
     public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public string Message => "Programador, fanatico de Star Wars y amante de la tecnologia.\n\n" +
+                             "Este es un proyecto de ejemplo para aprender a usar .NET MAUI, " +
+                             "creado por el programador Cisneros.\n\n" +
+                             "Para mas informacion visita el sitio web oficial de .NET MAUI.";
 }
